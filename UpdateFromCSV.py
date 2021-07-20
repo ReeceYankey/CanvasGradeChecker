e743b07099772982da945f7afba86f12583f6630
ReeceYankey <reeceyankey@gmail.com>
2021-07-20 10:25:35 -0400

    adding todos

--- UpdateFromCSV.py
+++ UpdateFromCSV.py
@@ -1,7 +1,8 @@
-from GradeSheets import *
+from GradeSheets import WeightedSheetHandler, PointSheetHandler
 import pandas as pd
 import openpyxl as xl
 
+#TODO does not support more than 10 categories of assignments
 
 def is_weighted_sheet(worksheet):
     return '(WP)' in worksheet['A2'].value
@@ -12,7 +13,7 @@ def update_from_csv(class_names):
     try:
         for name in class_names:
             table = pd.read_csv(f'ClassData\\{name}.csv')
-            print(type(table['type']))
+            # print(type(table['type']))
             # create new sheet if it doesn't exist
             if name not in wb:
                 inp = input("does " + name + " use weighted grades (w) or a point system (p)? ")
@@ -39,5 +40,7 @@ def update_from_csv(class_names):
 
 if __name__ == '__main__':
     #class_names = ['ENGE 1215', 'ENGR 1054', 'CHEM 1035', 'MATH 2204', 'CHEM 1045', 'GEOG 1014']
-    class_names = ['CS 2114']
+    class_names = ['CS 2114', 'GE 2019']
+    # TODO get all children of ClassData
+
     update_from_csv(class_names)
