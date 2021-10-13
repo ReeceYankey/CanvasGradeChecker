3a174999c36f55ee2ca414f70f3408ebbebeaf39
rpyankey <rpyankey@vt.edu>
2021-10-13 03:28:00 -0400

    windows fix

--- Setup.py
+++ Setup.py
@@ -1,5 +1,6 @@
-#from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 from sys import platform
+
 if platform == 'win32':
     from win32com.client import Dispatch
 from decouple import config
@@ -9,6 +10,7 @@ import re
 import zipfile
 import io
 import subprocess
+
 
 # class GenericInstaller(ABC):
 #     @classmethod
@@ -77,6 +79,7 @@ import subprocess
 #         return version
 
 def lookup_chrome_driver_version(chrome_version):
+    print(chrome_version)
     major_version = re.search(r'\d*(?=\.)', chrome_version).group(0)
 
     url = f"https://chromedriver.storage.googleapis.com/LATEST_RELEASE_{major_version}"
@@ -87,6 +90,7 @@ def lookup_chrome_driver_version(chrome_version):
 
     return r.text
 
+
 driver_url_LUT = {
     "linux": {
         "chrome": "https://chromedriver.storage.googleapis.com/{}/chromedriver_linux64.zip"
@@ -96,8 +100,10 @@ driver_url_LUT = {
     }
 }
 
+
 def get_driver_url(browser, driver_version):
     return driver_url_LUT[platform][browser].format(driver_version)
+
 
 def download_driver(browser, driver_version):
     url = get_driver_url(browser, driver_version)
@@ -114,11 +120,13 @@ def download_driver(browser, driver_version):
 
     print("successfully installed chromedriver")
 
-#---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
 def linux_get_chrome_version():
     completed_process = subprocess.run(['chromium', '--version'], check=True, capture_output=True, text=True)
     version = re.search(r"\d*\.\d*\.\d*\.\d*", completed_process.stdout).group(0)
     return version
+
 
 def win32_get_version_via_fs(filename):
     """get the version of chrome via the file system, given the path to chrome.exe"""
@@ -127,15 +135,21 @@ def win32_get_version_via_fs(filename):
     try:
         version = parser.GetFileVersion(filename)
     except Exception:
+        print("error")
         return None
+    print(f"version {version}")
     return version
+
 
 def win32_get_chrome_version():
     paths = [r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-            r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
-    version = [win32_get_version_via_fs(p) for p in paths if p is not None][0]
+             r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"]
+    version= list(filter(None, [win32_get_version_via_fs(p) for p in paths]))[0]
+    print(f"got version {version}")
     return version
-#---------------------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------------------
 
 func_get_chrome_version_LUT = {
     "linux": {
@@ -148,7 +162,7 @@ func_get_chrome_version_LUT = {
 driver_filename_LUT = {
     "linux": {
         "chrome": "chromedriver",
-        "firefox": "geckodriver" #FIXME doesn't work as a path
+        "firefox": "geckodriver"  # FIXME doesn't work as a path
     },
     "win32": {
         "chrome": "chromedriver.exe",
@@ -156,34 +170,39 @@ driver_filename_LUT = {
     }
 }
 
-#---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
 def install_driver(browser):
     get_chrome_version = func_get_chrome_version_LUT[platform][browser]
+    print(platform, browser, get_chrome_version)
     version = get_chrome_version()
+    print(version)
     driver_version = lookup_chrome_driver_version(version)
     # url = get_driver_url(browser, driver_version)
     download_driver(browser, driver_version)
-    return driver_filename_LUT[platform]
+    return driver_filename_LUT[platform]['chrome']
 
 
 def settings_file_exists():
     return os.path.isfile("settings.ini")
 
+
 def driver_path_is_valid():
     return os.path.isfile(config("DRIVER_PATH"))
+
 
 def first_time_setup():
     # user input
     print("Initiating first time setup...")
 
-    while(True):
+    while (True):
         try:
             inp = int(input("Please enter the browser you would like selenium to use (1: chrome, 2: firefox):"))
             if inp == 1:
-                PREFERRED_BROWSER='chrome'
+                PREFERRED_BROWSER = 'chrome'
                 break
             elif inp == 2:
-                PREFERRED_BROWSER='firefox'
+                PREFERRED_BROWSER = 'firefox'
                 break
         except ValueError:
             pass
@@ -194,11 +213,12 @@ def first_time_setup():
         if DRIVER_PATH == '':
             DRIVER_PATH = install_driver(PREFERRED_BROWSER)
     elif PREFERRED_BROWSER == 'firefox':
-        DRIVER_PATH = input("Please enter the full path (/full/path/to/geckodriver) of your geckodriver (downloadable at https://github.com/mozilla/geckodriver/releases/tag/latest):")
+        DRIVER_PATH = input(
+            "Please enter the full path (/full/path/to/geckodriver) of your geckodriver (downloadable at https://github.com/mozilla/geckodriver/releases/tag/latest):")
 
     CANVAS_USERNAME = input("Please enter your canvas username (optional):")
     CANVAS_PASSWORD = input("Please enter your canvas password (optional):")
-    
+
     # write settings.ini file
     settings = open("settings.ini", "w+")
     settings.write(("[settings]\n"
@@ -207,6 +227,7 @@ def first_time_setup():
                     "CANVAS_USERNAME={}\n"
                     "CANVAS_PASSWORD={}").format(PREFERRED_BROWSER, DRIVER_PATH, CANVAS_USERNAME, CANVAS_PASSWORD))
     settings.close()
+
 
 def verify_configuration():
     if not settings_file_exists():
